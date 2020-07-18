704f1b53ae4cd66a49533edb30c9671b5401b18d
Shahid N. Shah <shahid@shah.org>
2020-07-17 20:51:38 -0400

    Switch package proxy for deno-udd

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
-export * as contextMgr from "https://cdn.jsdelivr.net/gh/shah/context-manager@v1.0.2/mod.ts";
-export * as inflect from "https://cdn.jsdelivr.net/gh/shah/text-inflect@v1.0.3/mod.ts";
+export * as contextMgr from "https://denopkg.com/shah/context-manager@v1.0.4/mod.ts";
+export * as inflect from "https://denopkg.com/shah/text-inflect@v1.0.3/mod.ts";
 export * as stdCSV from "https://deno.land/std@v0.61.0/encoding/csv.ts";
 export * as stdBufIO from "https://deno.land/std@v0.61.0/io/bufio.ts";
 export * as stdAsserts from "https://deno.land/std@v0.61.0/testing/asserts.ts";
