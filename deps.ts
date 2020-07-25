cf82ed31cc1885f005952b5f960a16e891bc2f90
Shahid N. Shah <shahid@shah.org>
2020-07-25 10:46:30 -0400

    Upgrade Deno and stdlib

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
-export * as stdCSV from "https://deno.land/std@v0.61.0/encoding/csv.ts";
-export * as stdFS from "https://deno.land/std@v0.61.0/fs/mod.ts";
-export * as stdBufIO from "https://deno.land/std@v0.61.0/io/bufio.ts";
-export * as stdPath from "https://deno.land/std@v0.61.0/path/mod.ts";
-export * as stdAsserts from "https://deno.land/std@v0.61.0/testing/asserts.ts";
+export * as stdCSV from "https://deno.land/std@v0.62.0/encoding/csv.ts";
+export * as stdFS from "https://deno.land/std@v0.62.0/fs/mod.ts";
+export * as stdBufIO from "https://deno.land/std@v0.62.0/io/bufio.ts";
+export * as stdPath from "https://deno.land/std@v0.62.0/path/mod.ts";
+export * as stdAsserts from "https://deno.land/std@v0.62.0/testing/asserts.ts";
 export * as moment from "https://deno.land/x/moment/moment.ts";
 export * as contextMgr from "https://denopkg.com/shah/context-manager@v1.0.4/mod.ts";
 export * as inflect from "https://denopkg.com/shah/text-inflect@v1.0.3/mod.ts";
